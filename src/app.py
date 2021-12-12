eabbf20cfa14b5f97aa6a7b2e7f82fd7dda63149
Kyle Lewis <kylel9815@gmail.com>
2021-12-11 21:14:53 -0500

    Added Starting Summary Charts of All Billboard Attributes and General App Framework to Dash App

--- src/app.py
+++ src/app.py
@@ -14,7 +14,51 @@ external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
 
 years = [str(x) for x in range(1999, 2020, 1)]
+pie_parser = {"mode": {0: "minor", 1: "major"}, "key": {0:"C", 1:"C# / D-flat", 2:"D", 3:"D# / E-flat", 4:"E", 5:"F", 6:"F# / G-flat", 7: "G", 8:"G# / A-flat", 9:"A", 10:"A# / B-flat", 11:"B"},
+"timesignature": {3: "3/4", 4: "4/4", 5: "5/4", 1: "4/4", 0: "4/4"}}
 
+
+
+
+
+engine = sql.create_engine("sqlite:///./data/music.db")
+with engine.connect() as con:
+    billboard = pd.read_sql("SELECT * FROM billboard", con=con, parse_dates="WeekID")
+    attributes = pd.read_sql("SELECT * FROM attributes", con=con, parse_dates="release_date")
+    artists = pd.read_sql("SELECT * FROM artists", con=con)
+    recommend_join = pd.read_sql("SELECT * FROM recommendation_join", con=con)
+    artists_join = pd.read_sql("SELECT * FROM artists_join", con=con)
+
+dash_data = billboard.merge(attributes, "left", on="SongID").drop_duplicates("SongID").assign(count = 1).dropna()
+dash_data.loc[:, "explicit"] = dash_data.explicit.apply(lambda x: "explicit" if x == 1 else "clean")
+dash_data.loc[:, "timesignature"] = dash_data.timesignature.apply(lambda x: pie_parser["timesignature"][x])
+dash_data.loc[:, "key"] = dash_data.key.apply(lambda x: pie_parser["key"][x])
+dash_data.loc[:, "genre_super"] = dash_data.genre_super.apply(lambda x: "other" if x == "missing" or x == "empty" else x)
+dash_data = dash_data.rename(columns={"bill_popularity_y": "Chart Points"})
+dash_artists = billboard.merge(attributes, "left", on="SongID").merge(artists.merge(artists_join, "left", on=["artist_id"]), "left", left_on="id_y", right_on="song_id").drop_duplicates(["artist", "song_id"]).drop_duplicates(["artist_id"])
+
+fig = px.pie(dash_data, values="count", names="explicit", title= "% of Billboard Songs Explicit")
+fig2 = px.pie(dash_data, values="count", names="timesignature", title="Time Signatures of Billboard Songs")
+fig3 = px.pie(dash_data, values="count", names="key", title="Pitch Class of Billboard Songs")
+fig4 = px.pie(dash_data, values="count", names="genre_super", title="Major Genres of Billboard Songs")
+
+hist = px.histogram(dash_data, x="danceability")
+hist1 = px.histogram(dash_data, x="energy")
+hist2 = px.histogram(dash_data, x="valence")
+hist3 = px.histogram(dash_data, x="liveness")
+hist4 = px.histogram(dash_data, x="speechiness")
+hist5 = px.histogram(dash_data, x="acousticness")
+hist6 = px.histogram(dash_data, x="instrumentalness")
+hist7 = px.histogram(dash_data, x="duration")
+hist8 = px.histogram(dash_data, x="loudness")
+
+hist9 = px.histogram(dash_data, x="popularity")
+hist10 = px.histogram(dash_data, x="Chart Points")
+hist11 = px.histogram(dash_data, x="scaled_popularity")
+
+dash_artists.loc[:, "popularity_y"] = dash_artists.loc[:, "popularity_y"].astype("float")
+hist12 = px.histogram(dash_artists, x="popularity_y", title="Billboard Artist Popularity Distribution")
+hist13 = px.histogram(dash_artists, x="followers", title="Billboard Artist Followers Distribution")
 
 main_layout = html.Div([
     html.Div([
@@ -25,7 +69,41 @@ main_layout = html.Div([
             dbc.NavItem([dbc.Button(html.B('Debrief'), href='/debrief')])],
         brand="Home", brand_href="/"),
         dcc.Location(id='location'),
-        html.Div(id="analysis_content"),
+        dbc.Row([
+            dbc.Col(lg=1),
+            dbc.Col([
+                html.H1("Spotify Categorical Attributes"),
+                html.Br(),
+                dcc.Graph(figure=fig, id="explicit_pie"),
+                dcc.Graph(figure=fig2, id="timesig_pie"),
+                dcc.Graph(figure=fig3, id="key_pie"),
+                dcc.Graph(figure=fig4, id="genreSuper_pie"),
+                ], lg=4, style={"textAlign": "center"}),
+            dbc.Col([
+                html.H1("Spotify Interval Attributes Distribution"),
+                html.Br(),
+                dcc.Graph(figure=hist, id="dance_hist"),
+                dcc.Graph(figure=hist1, id="energy_hist"),
+                dcc.Graph(figure=hist2, id="valence_hist"),
+                dcc.Graph(figure=hist3, id="liveness_hist"),
+                dcc.Graph(figure=hist4, id="speechiness_hist"),
+                dcc.Graph(figure=hist5, id="acousticness_hist"),
+                dcc.Graph(figure=hist6, id="instrumentalness_hist"),
+                dcc.Graph(figure=hist7, id="duration_hist"),
+                dcc.Graph(figure=hist8, id="loudness_hist"),
+            ], lg=7, style={"textAlign": "center"}),
+            dbc.Col(lg=1),
+            dbc.Col([
+                html.H1("Popularity Then and Now"),
+                html.Br(),
+                dcc.Graph(figure=hist9, id="spot_pop_hist"),
+                dcc.Graph(figure=hist12, id="artist_pop_hist"),
+                dcc.Graph(figure=hist13, id="followers"),
+                dcc.Graph(figure=hist10, id="chart_pop_hist"),
+                dcc.Graph(figure=hist11, id="scaled_pop_hist"),
+                ], lg=11, style={"textAlign": "center"})
+            ],
+        id="analysis_content"),
         html.Br(),
         dbc.Row([
             dbc.Col(lg=1),
